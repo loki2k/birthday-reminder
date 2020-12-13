1c72fb9f4baceda94b3d8ccdffd5ff041082253f
loki2k <jeremylagarto@gmail.com>
2020-12-13 00:13:00 -0800

    Updates

---
+++ precache-manifest.0d5a5b50d4ad70f2b03d20fe1f5e360a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab16d983345aebf7505d9c713c34185",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "2a41b0e8102008c54f54",
+    "url": "/birthday-reminder/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "3ec3d1b5a9ffbb40ffa8",
+    "url": "/birthday-reminder/static/js/2.676e622f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.676e622f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a41b0e8102008c54f54",
+    "url": "/birthday-reminder/static/js/main.d50ad803.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
